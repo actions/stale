b8ef86dcfbd8563d927f871d9e4cb60e9695a106
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-01-16 22:16:11 +0100

    docs(date): add missing JSDoc about the return type

--- src/functions/dates/is-valid-date.ts
+++ src/functions/dates/is-valid-date.ts
@@ -6,6 +6,8 @@
  * https://stackoverflow.com/a/1353711/4440414
  *
  * @param {Readonly<Date>} date The date to check
+ *
+ * @returns {boolean} true when the given date is valid
  */
 export function isValidDate(date: Readonly<Date>): boolean {
   if (Object.prototype.toString.call(date) === '[object Date]') {

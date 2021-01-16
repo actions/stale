ba361e990ff34b43a3e613b0411685ad94362ca3
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-01-16 13:34:17 +0100

    chore: use camelCase for enum members

--- src/enums/issue-type.enum.ts
+++ src/enums/issue-type.enum.ts
@@ -1,4 +1,4 @@
 export enum IssueType {
-  ISSUE = 'issue',
-  PULL_REQUEST = 'pr'
+  Issue = 'issue',
+  PullRequest = 'pr'
 }
--- src/functions/get-issue-type.ts
+++ src/functions/get-issue-type.ts
@@ -1,5 +1,5 @@
 import {IssueType} from '../enums/issue-type.enum';
 
 export function getIssueType(isPullRequest: Readonly<boolean>): IssueType {
-  return isPullRequest ? IssueType.PULL_REQUEST : IssueType.ISSUE;
+  return isPullRequest ? IssueType.PullRequest : IssueType.Issue;
 }

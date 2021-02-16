2a88b666ebfd8064f1d22d28e981dde35ff98ebb
Jose Veiga <jose.veiga@swimlane.com>
2021-02-16 11:42:30 -0500

    chore: run pack script

--- dist/index.js
+++ dist/index.js
@@ -187,6 +187,12 @@ class IssuesProcessor {
                     issueLogger.info(`Skipping ${issueType} because it has an exempt label`);
                     continue; // don't process exempt issues
                 }
+                const anyOfLabels = words_to_list_1.wordsToList(this.options.anyOfLabels);
+                if (anyOfLabels.length &&
+                    !anyOfLabels.some((label) => is_labeled_1.isLabeled(issue, label))) {
+                    issueLogger.info(`Skipping ${issueType} because it does not have any of the required labels`);
+                    continue; // don't process issues without any of the required labels
+                }
                 const milestones = new milestones_1.Milestones(this.options, issue);
                 if (milestones.shouldExemptMilestones()) {
                     issueLogger.info(`Skipping ${issueType} because it has an exempt milestone`);
@@ -987,6 +993,7 @@ function _getAndValidateArgs() {
         closePrLabel: core.getInput('close-pr-label'),
         exemptPrLabels: core.getInput('exempt-pr-labels'),
         onlyLabels: core.getInput('only-labels'),
+        anyOfLabels: core.getInput('any-of-labels'),
         operationsPerRun: parseInt(core.getInput('operations-per-run', { required: true })),
         removeStaleWhenUpdated: !(core.getInput('remove-stale-when-updated') === 'false'),
         debugOnly: core.getInput('debug-only') === 'true',

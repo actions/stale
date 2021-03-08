b717aa9f47b3b780bbf93d496adc6f4c465fef82
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2021-03-08 11:56:52 +0100

    chore(logs): enhance the logs (#358)
    * docs(only-labels): enhance the docs and fix duplicate (#341)
    
    * docs(only-labels): remove duplicated option and improve descriptions
    
    a bad rebase happend
    
    * docs(readme): use a multi-line array and remove the optional column
    
    the option column was not helpful since each value is optional
    the multi-line array will allow to have a better UI in small devices and basically in GitHub too due to the max-width
    
    * style(readme): break line for the statistics
    
    * docs(readme): add a better description for the ascending option
    
    * docs(action): add missing punctuation
    
    * build(deps-dev): bump @typescript-eslint/eslint-plugin (#342)
    
    Bumps [@typescript-eslint/eslint-plugin](https://github.com/typescript-eslint/typescript-eslint/tree/HEAD/packages/eslint-plugin) from 4.15.2 to 4.16.1.
    - [Release notes](https://github.com/typescript-eslint/typescript-eslint/releases)
    - [Changelog](https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/CHANGELOG.md)
    - [Commits](https://github.com/typescript-eslint/typescript-eslint/commits/v4.16.1/packages/eslint-plugin)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * build(deps): bump @octokit/rest from 18.3.0 to 18.3.2 (#350)
    
    Bumps [@octokit/rest](https://github.com/octokit/rest.js) from 18.3.0 to 18.3.2.
    - [Release notes](https://github.com/octokit/rest.js/releases)
    - [Commits](https://github.com/octokit/rest.js/compare/v18.3.0...v18.3.2)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * test: add more coverage for the stale label behaviour (#352) (#15)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore(logs): add logs for the milestones
    
    * chore(errors): use actions error instead of throw errors
    
    * chore(logs): enhance the logs and add some colors
    
    tl;dr: blue for values, megenta for options, white for messages, yellow light for warnings, yellow for milestones and green for success
    still a WIP but I wish to confirm this before continuing
    @hross is it ok for you?
    
    * chore(index): update the index
    
    * chore(ci): use npm ci instead of npm i
    
    * chore(logs): removed some useless logs
    
    * refactor(issues): remove useless check
    
    * chore(statistics): show the real count of fetched issues
    
    * refactor(operations): use a class to handle the operations left
    
    closes #361
    
    * chore(logs): include the total number of issues in the log for a batch
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/classes/milestones.ts
+++ src/classes/milestones.ts
@@ -2,6 +2,7 @@ import deburr from 'lodash.deburr';
 import {wordsToList} from '../functions/words-to-list';
 import {IIssuesProcessorOptions} from '../interfaces/issues-processor-options';
 import {Issue} from './issue';
+import {IssueLogger} from './loggers/issue-logger';
 
 type CleanMilestone = string;
 
@@ -12,42 +13,158 @@ export class Milestones {
 
   private readonly _options: IIssuesProcessorOptions;
   private readonly _issue: Issue;
+  private readonly _issueLogger: IssueLogger;
 
   constructor(options: Readonly<IIssuesProcessorOptions>, issue: Issue) {
     this._options = options;
     this._issue = issue;
+    this._issueLogger = new IssueLogger(issue);
   }
 
   shouldExemptMilestones(): boolean {
+    if (!this._issue.milestone) {
+      this._issueLogger.info('This $$type has no milestone');
+      this._logSkip();
+
+      return false;
+    }
+
     if (this._shouldExemptAllMilestones()) {
+      this._issueLogger.info(
+        'Skipping $$type because it has an exempt milestone'
+      );
+
       return true;
     }
 
     const exemptMilestones: string[] = this._getExemptMilestones();
 
-    return exemptMilestones.some((exemptMilestone: Readonly<string>): boolean =>
-      this._hasMilestone(exemptMilestone)
+    if (exemptMilestones.length === 0) {
+      this._issueLogger.info(
+        `No milestone option was specified to skip the stale process for this $$type`
+      );
+      this._logSkip();
+
+      return false;
+    }
+
+    this._issueLogger.info(
+      `Found ${exemptMilestones.length} milestone${
+        exemptMilestones.length > 1 ? 's' : ''
+      } that can exempt stale on this $$type`
     );
+
+    const hasExemptMilestone: boolean = exemptMilestones.some(
+      (exemptMilestone: Readonly<string>): boolean =>
+        this._hasMilestone(exemptMilestone)
+    );
+
+    if (!hasExemptMilestone) {
+      this._issueLogger.info(
+        'No milestone on this $$type can exempt the stale process'
+      );
+      this._logSkip();
+    } else {
+      this._issueLogger.info(
+        'Skipping this $$type because it has an exempt milestone'
+      );
+    }
+
+    return hasExemptMilestone;
   }
 
   private _getExemptMilestones(): string[] {
-    return wordsToList(
-      this._issue.isPullRequest
-        ? this._getExemptPullRequestMilestones()
-        : this._getExemptIssueMilestones()
+    return this._issue.isPullRequest
+      ? this._getExemptPullRequestMilestones()
+      : this._getExemptIssueMilestones();
+  }
+
+  private _getExemptIssueMilestones(): string[] {
+    if (this._options.exemptIssueMilestones === '') {
+      this._issueLogger.info(
+        'The option "exemptIssueMilestones" is disabled. No specific milestone can skip the stale process for this $$type'
+      );
+
+      if (this._options.exemptMilestones === '') {
+        this._issueLogger.info(
+          'The option "exemptMilestones" is disabled. No specific milestone can skip the stale process for this $$type'
+        );
+
+        return [];
+      }
+
+      const exemptMilestones: string[] = wordsToList(
+        this._options.exemptMilestones
+      );
+
+      this._issueLogger.info(
+        `The option "exemptMilestones" is set. ${
+          exemptMilestones.length
+        } milestone${
+          exemptMilestones.length === 1 ? '' : 's'
+        } can skip the stale process for this $$type`
+      );
+
+      return exemptMilestones;
+    }
+
+    const exemptMilestones: string[] = wordsToList(
+      this._options.exemptIssueMilestones
     );
+
+    this._issueLogger.info(
+      `The option "exemptIssueMilestones" is set. ${
+        exemptMilestones.length
+      } milestone${
+        exemptMilestones.length === 1 ? '' : 's'
+      } can skip the stale process for this $$type`
+    );
+
+    return exemptMilestones;
   }
 
-  private _getExemptIssueMilestones(): string {
-    return this._options.exemptIssueMilestones !== ''
-      ? this._options.exemptIssueMilestones
-      : this._options.exemptMilestones;
-  }
+  private _getExemptPullRequestMilestones(): string[] {
+    if (this._options.exemptPrMilestones === '') {
+      this._issueLogger.info(
+        'The option "exemptPrMilestones" is disabled. No specific milestone can skip the stale process for this $$type'
+      );
 
-  private _getExemptPullRequestMilestones(): string {
-    return this._options.exemptPrMilestones !== ''
-      ? this._options.exemptPrMilestones
-      : this._options.exemptMilestones;
+      if (this._options.exemptMilestones === '') {
+        this._issueLogger.info(
+          'The option "exemptMilestones" is disabled. No specific milestone can skip the stale process for this $$type'
+        );
+
+        return [];
+      }
+
+      const exemptMilestones: string[] = wordsToList(
+        this._options.exemptMilestones
+      );
+
+      this._issueLogger.info(
+        `The option "exemptMilestones" is set. ${
+          exemptMilestones.length
+        } milestone${
+          exemptMilestones.length === 1 ? '' : 's'
+        } can skip the stale process for this $$type`
+      );
+
+      return exemptMilestones;
+    }
+
+    const exemptMilestones: string[] = wordsToList(
+      this._options.exemptPrMilestones
+    );
+
+    this._issueLogger.info(
+      `The option "exemptPrMilestones" is set. ${
+        exemptMilestones.length
+      } milestone${
+        exemptMilestones.length === 1 ? '' : 's'
+      } can skip the stale process for this $$type`
+    );
+
+    return exemptMilestones;
   }
 
   private _hasMilestone(milestone: Readonly<string>): boolean {
@@ -55,10 +172,21 @@ export class Milestones {
       return false;
     }
 
-    return (
-      Milestones._cleanMilestone(milestone) ===
-      Milestones._cleanMilestone(this._issue.milestone.title)
+    const cleanMilestone: CleanMilestone = Milestones._cleanMilestone(
+      milestone
     );
+
+    const isSameMilestone: boolean =
+      cleanMilestone ===
+      Milestones._cleanMilestone(this._issue.milestone.title);
+
+    if (isSameMilestone) {
+      this._issueLogger.info(
+        `The milestone "${milestone}" is set on this $$type and is an exempt milestone`
+      );
+    }
+
+    return isSameMilestone;
   }
 
   private _shouldExemptAllMilestones(): boolean {
@@ -73,21 +201,57 @@ export class Milestones {
 
   private _shouldExemptAllIssueMilestones(): boolean {
     if (this._options.exemptAllIssueMilestones === true) {
+      this._issueLogger.info(
+        'The option "exemptAllIssueMilestones" is enabled. Any milestone on this $$type will skip the stale process'
+      );
+
       return true;
     } else if (this._options.exemptAllIssueMilestones === false) {
+      this._issueLogger.info(
+        'The option "exemptAllIssueMilestones" is disabled. Only some specific milestones on this $$type will skip the stale process'
+      );
+
       return false;
     }
+
+    this._logExemptAllMilestonesOption();
 
     return this._options.exemptAllMilestones;
   }
 
   private _shouldExemptAllPullRequestMilestones(): boolean {
     if (this._options.exemptAllPrMilestones === true) {
+      this._issueLogger.info(
+        'The option "exemptAllPrMilestones" is enabled. Any milestone on this $$type will skip the stale process'
+      );
+
       return true;
     } else if (this._options.exemptAllPrMilestones === false) {
+      this._issueLogger.info(
+        'The option "exemptAllPrMilestones" is disabled. Only some specific milestones on this $$type will skip the stale process'
+      );
+
       return false;
     }
 
+    this._logExemptAllMilestonesOption();
+
     return this._options.exemptAllMilestones;
+  }
+
+  private _logExemptAllMilestonesOption(): void {
+    if (this._options.exemptAllMilestones) {
+      this._issueLogger.info(
+        'The option "exemptAllMilestones" is enabled. Any milestone on this $$type will skip the stale process'
+      );
+    } else {
+      this._issueLogger.info(
+        'The option "exemptAllMilestones" is disabled. Only some specific milestones on this $$type will skip the stale process'
+      );
+    }
+  }
+
+  private _logSkip(): void {
+    this._issueLogger.info('Skip the milestones checks');
   }
 }

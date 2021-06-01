369f8ff65f0585ec9adb84b306528529e7b915de
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-06-01 21:25:18 +0200

    chore(main): add 3 new options

--- __tests__/constants/default-processor-options.ts
+++ __tests__/constants/default-processor-options.ts
@@ -46,5 +46,8 @@ export const DefaultProcessorOptions: IIssuesProcessorOptions = Object.freeze({
   exemptAllPrAssignees: undefined,
   enableStatistics: true,
   labelsToRemoveWhenUnstale: '',
-  labelsToAddWhenUnstale: ''
+  labelsToAddWhenUnstale: '',
+  ignoreAllActivitiesBeforeStale: false,
+  ignoreAllIssueActivitiesBeforeStale: undefined,
+  ignoreAllPrActivitiesBeforeStale: undefined
 });

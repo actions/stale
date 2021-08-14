7bfcefbbf69b5d34e11bfbe67546a671c1d66d51
Geoffrey Testelin <geoffrey.testelin@gmail.com>
2021-08-14 14:56:22 +0200

    chore(assignees): add logs

--- src/classes/issues-processor.ts
+++ src/classes/issues-processor.ts
@@ -855,7 +855,11 @@ export class IssuesProcessor {
   private async _deleteBranch(issue: Issue): Promise<void> {
     const issueLogger: IssueLogger = new IssueLogger(issue);
 
-    issueLogger.info(`Delete branch from closed $$type - ${issue.title}`);
+    issueLogger.info(`Delete
+    branch from closed $
+    $type
+    -
+    ${issue.title}`);
 
     const pullRequest = await this._getPullRequest(issue);
 

966b0db1125ec990fb2940f37a5db70783e0209e
TESTELIN Geoffrey <geoffrey.testelin@gmail.com>
2021-09-14 23:30:18 +0200

    chore(index): update index file

--- dist/index.js
+++ dist/index.js
@@ -9099,12 +9099,12 @@ module.exports = require("zlib");;
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
-/******/
+/******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module is referenced by other modules so it can't be inlined
 /******/ 	var __webpack_exports__ = __nccwpck_require__(3109);
 /******/ 	module.exports = __webpack_exports__;
-/******/
+/******/ 	
 /******/ })()
 ;

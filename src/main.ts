7e06b131d5dc41c6fbe67df1e6aef39024ee0ff4
zhews <118117386+zhews@users.noreply.github.com>
2024-08-14 15:54:08 +0200

    Allow the processing of locked issues and pull requests

--- src/main.ts
+++ src/main.ts
@@ -123,7 +123,8 @@ function _getAndValidateArgs(): IIssuesProcessorOptions {
     ignorePrUpdates: _toOptionalBoolean('ignore-pr-updates'),
     exemptDraftPr: core.getInput('exempt-draft-pr') === 'true',
     closeIssueReason: core.getInput('close-issue-reason'),
-    includeOnlyAssigned: core.getInput('include-only-assigned') === 'true'
+    includeOnlyAssigned: core.getInput('include-only-assigned') === 'true',
+    exemptLocked: core.getInput('exempt-locked') === 'true'
   };
 
   for (const numberInput of ['days-before-stale']) {

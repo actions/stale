cfb6febb763634c4b39f80c672097702eb4635bd
Tyler Michael <tylermichael@users.noreply.github.com>
2024-10-02 22:27:30 -0400

    add any-of-authors param

--- src/main.ts
+++ src/main.ts
@@ -108,6 +108,7 @@ function _getAndValidateArgs(): IIssuesProcessorOptions {
     exemptAllMilestones: core.getInput('exempt-all-milestones') === 'true',
     exemptAllIssueMilestones: _toOptionalBoolean('exempt-all-issue-milestones'),
     exemptAllPrMilestones: _toOptionalBoolean('exempt-all-pr-milestones'),
+    anyOfAuthors: core.getInput('any-of-authors'),
     exemptAssignees: core.getInput('exempt-assignees'),
     exemptIssueAssignees: core.getInput('exempt-issue-assignees'),
     exemptPrAssignees: core.getInput('exempt-pr-assignees'),

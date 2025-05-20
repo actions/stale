a92e1039a5f7373b2eab1e4c97816a39a0358385
suyashgaonkar <suyashgaonkar@github.com>
2025-05-20 14:12:58 +0530

    final draft of sort-issues-by

--- src/classes/issue.spec.ts
+++ src/classes/issue.spec.ts
@@ -13,7 +13,7 @@ describe('Issue', (): void => {
   beforeEach((): void => {
     optionsInterface = {
       ascending: false,
-      sortIssuesBy:'created',
+      sortIssuesBy: 'created',
       closeIssueLabel: '',
       closeIssueMessage: '',
       closePrLabel: '',
